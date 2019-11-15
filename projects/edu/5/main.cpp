395b771ac3dee014e938afc6b3405663570b2858
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-15 11:07:09 +0100

    bm: shell file and data

---
+++ projects/edu/5/main.cpp
@@ -0,0 +1,35 @@
+/* 
+question
+=====================================================
+Write a C++ application that calculates the wages of staff for a company called EPSILON
+computers. The are two categories of staff in the company, junior and senior staff. The
+normal working hours for any staff is 40 hours. The hourly rate for overtime is 1.5 times the
+normal rate for junior staff and 2.0 times the normal rate for senior staff. Note that no one is
+allowed to do more than maximum of 70 hours per week. The normal pay rate for junior
+staff is 200 naira per hour for junior staff and 300 naira per hour for senior staff. Your
+application must be able to read information from a file and write output into a file. Test
+with 50 staff.
+====================================================
+*/
+
+#include <cstdlib>
+#include <iostream>
+
+/*
+*** EPSILON Wage Calculator ***
+* types of staff
+  - junior staff
+  - senior staff
+* normal working hours - 40
+* maximum working hours - 70
+* max overtime hours - 30
+* normal hours rate (j, s) - (200, 300)
+* overtime hours (j, s) - (1.5 * 200, 2.0 * 300)
+* read from file, output from file
+* minimum 50 staff
+*/
+
+
+void main() {
+
+}

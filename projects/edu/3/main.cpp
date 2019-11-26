685de56023ed347101dbd652d0998fe6890f4e60
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-26 23:04:46 +0100

    BM: TEMP DATA set

---
+++ projects/edu/3/main.cpp
@@ -0,0 +1,26 @@
+/* 
+  Aggregation - UTME, LASU SCORE, WAEC RESULT
+  cut-off = 52%;
+  max p-utme = 50, max utme = 400
+  O/L result from 5 best subjects
+
+  Aggregate = 0.0625 * UME score + 0.50 * GCE-O/L score + 0.50 * post–UME score
+  O/L score = Total O/L point + Sitting point
+  C6 = 3, C5 = 4, C4 = 5, B3 (A3) = 6, B2 (A2) = 7, A1 = 8 
+  max sitting = 2
+  1 sitting = 10 points 
+  2 sittings = 5 points
+
+  100 students
+  read from file and write to file
+*/
+
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  const string header = "S/N,NAME,AGGREGATE,ADMISSION STATUS";
+  // receive file path relative to main.cpp
+}

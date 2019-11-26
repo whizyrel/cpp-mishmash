fb7b0eb9411816eb384cf6dd7e488ac9fc48f39e
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-26 05:09:46 +0100

    bm: inserted header member

--- projects/edu/5/headers/_essentials.h
+++ projects/edu/5/headers/_essentials.h
@@ -12,5 +12,30 @@ namespace _essentials {
   {
     struct stat buffer;
     return (stat(path.c_str(), &buffer) == 0);
+  };
+
+  void read_file(std::string path)
+  {
+
+  };
+
+  bool write_header(std::string text, std::string fn)
+  {
+    std::fstream fs;
+
+    fs.open(fn);
+
+    if (fs.is_open())
+    {
+      fs << text << std::endl;
+      fs.close();
+    } else
+    {
+      std::cout << "file opening failed, check file again" << std::endl;
+      
+      fs.close();
+    }
+
+    return fs.is_open();
   }
 }

b456310ea2682ca72561a1ebfbe3d1a0fdad5d68
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-27 07:15:44 +0100

    bm: headers, wow... lost

--- projects/edu/3/main.cpp
+++ projects/edu/3/main.cpp
@@ -1,12 +1,13 @@
 /* 
   Aggregation - UTME, LASU SCORE, WAEC RESULT
   cut-off = 52%;
-  max p-utme = 50, max utme = 400
+  max post_utme = 50, max utme = 400
   O/L result from 5 best subjects
 
   Aggregate = 0.0625 * UME score + 0.50 * GCE-O/L score + 0.50 * post–UME score
+
   O/L score = Total O/L point + Sitting point
-  C6 = 3, C5 = 4, C4 = 5, B3 (A3) = 6, B2 (A2) = 7, A1 = 8 
+  C6 = 3, C5 = 4, C4 = 5, B3 (A3) = 6, B2 (A2) = 7, A1 = 8
   max sitting = 2
   1 sitting = 10 points 
   2 sittings = 5 points
@@ -15,12 +16,93 @@
   read from file and write to file
 */
 
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
 #include <string>
+#include <bits/stdc++.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 using namespace std;
 
 int main()
 {
+  fstream rfs; // result file opening fstream
+
   const string header = "S/N,NAME,AGGREGATE,ADMISSION STATUS";
+  const string filepath = "./resources/results.csv";
+  const string results_dir = "scores";
+  string line;
+
+  int count = 0, sitting_no;
+
+  cout << "*****************************************************************" << endl;
+  cout << "                         Admission Module                        " << endl;
+  cout << "preparing application environment" << endl;
+
+  mkdir(results_dir.c_str(), 0777);
+
   // receive file path relative to main.cpp
+  // receive folder directory to save file
+  // score filename
+  // receive number of sittings
+  cout << "How many sittings (maximum no of sittings is 2): " << sitting_no << endl;
+
+  // open file for reading
+  rfs.open(filepath);
+
+  if (rfs.is_open())
+  {
+    cout << "file was opened successfully!" << endl;
+
+    // read file
+    for (count, line; getline(rfs, line, '\n'); count++)
+    {
+      // cout << "line: " << line << endl;
+      string name, ol_result;
+      int utme, post_utme;
+
+      stringstream lfs(line); // line string stream
+
+      if (count != 0)
+      {
+        // split line by comma
+        string row;
+        int i;
+
+        for (row, i = 0; getline(lfs, row, ','); i++)
+        {
+          cout << "each row: " << row << endl;
+
+          if (i == 1)
+          {
+            name = row;
+          }
+
+          if (i == 2)
+          {
+            ol_result = row;
+          }
+
+          if (i == 3)
+          {
+            utme = atol(row.c_str());
+          }
+
+          if (i == 4)
+          {
+            post_utme = atol(row.c_str());
+          }
+        }
+      }
+
+      // split cols, split result
+    }
+    } else 
+  {
+    cout << "Error: file was not opened successfully" << endl;
+  }
+
+  rfs.close();
 }

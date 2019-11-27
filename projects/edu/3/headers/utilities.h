abd22df1bda19440e84869b0900ec515e6b6f65e
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-27 07:15:14 +0100

    bm: partial fn def, additional parameter insertion

--- projects/edu/3/headers/utilities.h
+++ projects/edu/3/headers/utilities.h
@@ -1,4 +1,7 @@
+#include <cstdlib>
 #include <string>
+#include <iostream>
+#include <fstream>
 
 namespace utilities
 {
@@ -6,9 +9,18 @@ namespace utilities
   {
     // split string
     // calculate points
+    std::fstream lfs;
+    std::string chunk;
+    int i;
+
+    for (chunk, i = 0; getline(lfs, chunk, ';'); i++)
+    {
+      std::stringstream sss(chunk);
+      
+    }
   }
 
-  double calc_aggregate(int utme, int post_utme, int ol_score)
+  double calc_aggregate(int utme, int post_utme, int ol_score, int sitting_no)
   {
     // calculate aggregate
 
@@ -21,6 +33,13 @@ namespace utilities
     if (post_utme > 50)
     {
       post_utme = 50;
+    }
+    
+    if (sitting_no > 2)
+    {
+      std::cout << "Max no of sitting is 2, you have not qualified for admission!" << std::endl;
+
+      return;
     }
 
     return (0.0625 * utme) + (0.50 * ol_score) + (0.50 * post_utme);

37cb826da47b7131e81e43b38eaca7d9f104e795
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-27 16:26:25 +0100

    bm: imported header

---
+++ projects/edu/3/headers/_essentials.h
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+namespace _essentials {
+  bool pathExists(const std::string &path)
+  {
+    struct stat buffer;
+    return (stat(path.c_str(), &buffer) == 0);
+  };
+
+  void read_file(std::string path)
+  {
+
+  };
+
+  bool write_header(std::string text, std::string fn)
+  {
+    std::fstream fs;
+
+    fs.open(fn);
+
+    if (fs.is_open())
+    {
+      fs << text << std::endl;
+      fs.close();
+    } else
+    {
+      std::cout << "file opening failed, check file again" << std::endl;
+      
+      fs.close();
+    }
+
+    return fs.is_open();
+  }
+}
